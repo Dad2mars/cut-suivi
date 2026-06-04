ddc39af040dd47f0c5defef46e460e6b856c295f
Dad2mars <dad.taxi@gmail.com>
2026-06-04 21:39:17 +0200

    v17 - refonte ergonomique (onglets, ajout unifie, habituels, objectifs)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cut-v16";
+const CACHE = "cut-v17";
 const SHELL = ["./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
