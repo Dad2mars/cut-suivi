00b4c60bd19b69dcf5b6edb74ed47740b278d643
Dad2mars <dad.taxi@gmail.com>
2026-06-04 21:49:39 +0200

    v18 - navigation jours + graphique calories 14j

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cut-v17";
+const CACHE = "cut-v18";
 const SHELL = ["./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
