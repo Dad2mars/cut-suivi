4ca3503f8f2302e1e807b67590e6e925964a32ca
Dad2mars <dad.taxi@gmail.com>
2026-06-04 18:19:27 +0200

    v14 - instructions claires connexion app installee

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cut-v13";
+const CACHE = "cut-v14";
 const SHELL = ["./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
