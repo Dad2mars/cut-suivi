03961e3157281426fc35bf8adbea1e98d87a9417
Dad2mars <Dad2mars@users.noreply.github.com>
2026-06-11 07:33:51 +0200

    v24 - journal des pesees (valeurs visibles, edit/suppr) + graphe poids ameliore (periode 7/30/90/tout, ligne objectif, moyenne lissee)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cut-v23";
+const CACHE = "cut-v24";
 const SHELL = ["./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
