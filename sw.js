cc7c6357ff7a2f9eda97c9257356e6e556e4f0b2
Dad2mars <Dad2mars@users.noreply.github.com>
2026-06-28 11:28:13 +0200

    v27 - corrige la date de build (28/06/2026)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cut-v26";
+const CACHE = "cut-v27";
 const SHELL = ["./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
