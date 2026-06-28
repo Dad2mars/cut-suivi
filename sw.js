11b4eadbaac191f75dff768c0485b7769f71ec8e
Dad2mars <Dad2mars@users.noreply.github.com>
2026-06-28 11:32:30 +0200

    v28 - mise a jour automatique de la PWA (auto-reload au nouveau service worker)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cut-v27";
+const CACHE = "cut-v28";
 const SHELL = ["./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
