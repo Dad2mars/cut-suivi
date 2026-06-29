bd397ae060d1738c81d84ef264c52d456e344dd5
Dad2mars <Dad2mars@users.noreply.github.com>
2026-06-29 10:37:36 +0200

    v30 - journal des pesees = historique complet (toutes les pesees, non filtre par periode)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cut-v29";
+const CACHE = "cut-v30";
 const SHELL = ["./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
